c3d3f7406a61bfce6509c97cc739c7ad1bfcf595
hafizswabah <swabah.a.avd121@gmail.com>
2023-03-14 09:46:26 +0530

    changes

--- app.js
+++ app.js
@@ -12,19 +12,22 @@ const userRouter = require('./Router/UserRouter')
 const adminRouter = require('./Router/adminRouter')
 
 const path = require("path")
-
+ 
 app.set('view engine', 'ejs')
-// app.use(expresslayouts)
-// app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, "/public")))
 mongodb()
+
+app.get("/check",(req,res)=>{
+    console.log("version 1 running")
+})
+
 app.use(session({
     secret: 'key',
     resave: false,
     saveUninitialized: true,
-    store: MongoStore.create({ mongoUrl: 'mongodb://127.0.0.1:27017/GCART'})
+    store: MongoStore.create({ mongoUrl: process.env.DB_CONFIG})
 }));
 app.use(function (req, res, next) {
     res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
